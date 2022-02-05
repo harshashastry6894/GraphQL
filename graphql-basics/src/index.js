f85df5f575ae39e95357e8683b4ad57893ae40bc
Harsha S <harsha.s@netapp.com>
2022-02-05 20:51:45 +0530

    relational data basics

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -6,10 +6,12 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
     type Query {
         greeting(name: String): String!
-        me: User!
         grades: [Int!]!
         add(numbers: [Float!]): Float!
+        me: User!
+        post: Post!
         users(query: String): [User!]!
+        posts(query: String): [Post!]!
     }
 
     type User {
@@ -18,7 +20,49 @@ const typeDefs = `
         email: String!
         age: Int
     }
+
+    type Post {
+        id: ID!
+        title: String!
+        body: String!
+        published: Boolean!
+        author: User!
+    }
 `
+const users = [{
+    id: '1',
+    name: 'Andrew',
+    email: 'andrew@example.com',
+    age: 27
+}, {
+    id: '2',
+    name: 'Sarah',
+    email: 'sarah@example.com'
+}, {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@example.com'
+}]
+
+const posts = [{
+    id: '10',
+    title: 'GraphQL 101',
+    body: 'This is how to use GraphQL...',
+    published: true,
+    author: '1'
+}, {
+    id: '11',
+    title: 'GraphQL 201',
+    body: 'This is an advanced GraphQL post...',
+    published: false,
+    author: '2'
+}, {
+    id: '12',
+    title: 'Programming Music',
+    body: '',
+    published: false,
+    author: '3'
+}]
 
 // Resolvers
 const resolvers = {
@@ -49,32 +93,37 @@ const resolvers = {
             }
         },
         users(parent, args, ctx, info) {
-            const users = [{
-                id: '1',
-                name: 'John',
-                email: 'john@example.com',
-                age: 18
-            },
-            {
-                id: '2',
-                name: 'Harsha',
-                email: 'Harsha@example.com',
-                age: 22
-            },
-            {
-                id: '3',
-                name: 'Mike',
-                email: 'Mike@example.com',
-                age: 22
-            }
-            ]
-            
             if(!args.query) {
                 return users
             } else {
                 return users.filter(user => user.name.toLowerCase() === args.query.toLowerCase())
             }
-            
+        },
+        posts(parent, args, ctx, info) {
+            if (!args.query) {
+                return posts
+            }
+
+            return posts.filter((post) => {
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                return isTitleMatch || isBodyMatch
+            })
+        },
+        post() {
+            return {
+                id: '092',
+                title: 'GraphQL 101',
+                body: '',
+                published: false,
+                author: '1'
+            }
+        }
+        
+    },
+    Post: {
+        author(parent, args, ctx, info) {
+           return users.find((user) => user.id === parent.author) 
         }
     }
 }
