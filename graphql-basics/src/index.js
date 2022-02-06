4ff0c2783a01be0236c902d0c57a2be1c6b8c33b
Harsha S <harsha.s@netapp.com>
2022-02-06 12:54:36 +0530

    Relational data arrays

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -19,6 +19,7 @@ const typeDefs = `
         name: String!
         email: String!
         age: Int
+        posts: [Post!]
     }
 
     type Post {
@@ -124,6 +125,11 @@ const resolvers = {
     Post: {
         author(parent, args, ctx, info) {
            return users.find((user) => user.id === parent.author) 
+        }
+    },
+    User: {
+        posts(parent, args, ctx, info) {
+            return posts.filter(post => post.author === parent.id)
         }
     }
 }
